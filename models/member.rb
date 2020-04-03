3ca2181b00639635c78e5d74e31ca2fbbe3ffc83
Tony Tang <teokigi@hotmail.com>
2020-04-03 20:40:54 +0100

    coded register method

--- models/member.rb
+++ models/member.rb
@@ -2,6 +2,9 @@ require_relative('../db/sqlrunner.rb')
 
 class Member
 
+		attr_accessor :status, :first_name, :last_name, :account_type
+		attr_reader :id
+
 	def initialize(options)
 		@status = 'active'
 		@first_name = options['first_name']
@@ -21,6 +24,11 @@ class Member
 				VALUES
 				(
 					$1,$2,$3,$4
-				)"
+				)
+				RETURNING id"
+		values = [@status,@first_name,@last_name,@account_type]
+		returning_id = Sqlrunner.run(sql,values)
+		returning_id = returning_id.first()
+		@id = returning_id['id'].to_i
 	end
 end
