a6f83cabc38ca5bc91006ab82d4fa17b8a2daff2
Tony Tang <teokigi@hotmail.com>
2020-04-03 20:48:49 +0100

    created file testing for classes

---
+++ db/console.rb
@@ -0,0 +1,9 @@
+require('pry-byebug')
+require_relative('../models/booking.rb')
+require_relative('../models/gym_class.rb')
+require_relative('../models/member.rb')
+require_relative('../models/session.rb')
+
+
+binding.pry
+nil
