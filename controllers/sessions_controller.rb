4e53113954429df2c32789938fbb404904ec8d4e
Tony Tang <teokigi@hotmail.com>
2020-04-05 21:42:29 +0100

    done with gym classes onto the sessions page

--- controllers/sessions_controller.rb
+++ controllers/sessions_controller.rb
@@ -2,7 +2,8 @@ require( 'sinatra' )
 require( 'sinatra/contrib/all' )
 also_reload( '../models/*' )
 
-get '/sessions' do
-	# @all_sessions = Sessions.find_all
+get '/sessions/:class_id/index' do
+	@class_id = params['class_id'].to_i
+	@sessions = Sessions.find_by_gym_class_id(@class_id)
 	erb(:"/sessions/index")
 end
