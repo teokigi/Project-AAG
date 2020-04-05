2ac5c05e3790ea102127fd089e435ba170be6652
Tony Tang <teokigi@hotmail.com>
2020-04-05 22:48:47 +0100

    session page now working

--- controllers/sessions_controller.rb
+++ controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
-require_relative('../models/sessions.rb')
+require_relative('../models/session.rb')
 also_reload( '../models/*' )
 
 get '/sessions/:class_id/index' do
 	@class_id = params['class_id'].to_i
-	@sessions = Sessions.find_sessions_with_gym_class_id(@class_id)
+	@sessions = Session.find_sessions_with_gym_class_id(@class_id)
 	erb(:"sessions/index")
 end
