c12f7fac0e345bcfe6da3e6456ba162d3a5cc12f
Tony Tang <teokigi@hotmail.com>
2020-04-05 21:06:44 +0100

    made the gym class index page, able to generate new classes

--- controllers/gym_classes_controller.rb
+++ controllers/gym_classes_controller.rb
@@ -1,8 +1,15 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
+require_relative('../models/gym_class.rb')
 also_reload( '../models/*' )
 
 get '/gym_classes' do
-	# @classes = GymClasses.find_all
+	@classes = GymClass.find_all
 	erb(:"gym_classes/index")
+end
+
+post '/gym_classes' do
+	new_class = GymClass.new(params)
+	new_class.create
+	redirect to("/gym_classes")
 end
