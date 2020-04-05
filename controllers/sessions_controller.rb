e205d7658f3c06601e6c4b374264db5eab082fe4
Tony Tang <teokigi@hotmail.com>
2020-04-05 22:47:13 +0100

    require relative added to sessions controller

--- controllers/sessions_controller.rb
+++ controllers/sessions_controller.rb
@@ -1,9 +1,10 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
+require_relative('../models/sessions.rb')
 also_reload( '../models/*' )
 
 get '/sessions/:class_id/index' do
 	@class_id = params['class_id'].to_i
 	@sessions = Sessions.find_sessions_with_gym_class_id(@class_id)
-	erb(:"/sessions/index")
+	erb(:"sessions/index")
 end
