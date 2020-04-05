14d194bcabb48bf453b101f7528f71898cedaa0a
Tony Tang <teokigi@hotmail.com>
2020-04-06 00:37:06 +0100

    sessions fully functionally, link to bookings working

--- controllers/bookings_controller.rb
+++ controllers/bookings_controller.rb
@@ -1,6 +1,6 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
-require_relative('../models/customer.rb')
+require_relative('../models/member.rb')
 require_relative('../models/session.rb')
 require_relative('../models/gym_class.rb')
 require_relative('../models/booking.rb')
@@ -9,12 +9,7 @@ also_reload( '../models/*' )
 get '/bookings/:session_id/index' do
 	@members = Member.find_all
 	@session = Session.find_by_id(params['session_id'].to_i)
-	@gym_class = GymClass.find_by_id(session.gym_class_id)
-	@all_bookings = Bookings.find_all_bookings_by_session_id(params['session_id'].to_i)
-	erb(:"/bookings/#{params['session_id']}/index")
-end
-get '/sessions/:class_id/index' do
-
-	@sessions = Session.find_sessions_with_gym_class_id(@gym_class.id)
-	erb(:"sessions/index")
+	@gym_class = GymClass.find_by_id(@session.gym_class_id)
+	@all_bookings = Booking.find_bookings_with_session_id(params['session_id'].to_i)
+	erb(:"/bookings/index")
 end
