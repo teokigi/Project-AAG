cc19c0278521b4c654bb1598c3c06d94381c9a21
Tony Tang <teokigi@hotmail.com>
2020-04-06 12:30:46 +0100

    removed status from 2 tables, created seed data in sql file

--- controllers/bookings_controller.rb
+++ controllers/bookings_controller.rb
@@ -6,11 +6,11 @@ require_relative('../models/gym_class.rb')
 require_relative('../models/booking.rb')
 also_reload( '../models/*' )
 
-get '/bookings/:session_id/index' do
+get '/bookings' do
+	@bookings = Booking.find_all
 	@members = Member.find_all
-	@session = Session.find_by_id(params['session_id'].to_i)
-	@gym_class = GymClass.find_by_id(@session.gym_class_id)
-	@bookings = Booking.find_bookings_with_session_id(params['session_id'].to_i)
+	@Gym_classes = GymClass.find_all
+	@sessions = Session.find_all
 	erb(:"/bookings/index")
 end
 
--- db/athletes_assemble_gym.sql
+++ db/athletes_assemble_gym.sql
@@ -18,7 +18,6 @@ CREATE TABLE gym_classes(
 
 CREATE TABLE sessions(
 	id SERIAL PRIMARY KEY,
-	status VARCHAR(255),
 	gym_class_id INT REFERENCES gym_classes(id) ON DELETE CASCADE,
 	time_slot VARCHAR(255),
 	maximum_bookings INT,
@@ -27,7 +26,54 @@ CREATE TABLE sessions(
 
 CREATE TABLE bookings(
 	id SERIAL PRIMARY KEY,
-	status VARCHAR(255),
 	session_id INT REFERENCES sessions(id) ON DELETE CASCADE,
 	member_id INT REFERENCES members(id) ON DELETE CASCADE
 );
+
+INSERT INTO members (id,status,first_name,last_name,account_type) VALUES
+	(1,'active','Allan','Atkinson','standard'),
+	(2,'active','Brian','Bedford','standard'),
+	(3,'active','Charlie','Carlson','standard'),
+	(4,'active','David','Davidson','standard'),
+	(5,'active','Edward','Enders','standard'),
+	(6,'active','Fred','Flanders','standard'),
+	(7,'active','Greg','Granger','standard'),
+	(8,'active','Holly','Hanson','premium'),
+	(9,'active','Irene','Idlyeshire','premium'),
+	(10,'inactive','Jake','Jackson','standard');
+
+INSERT INTO gym_classes (id,status,name) VALUES
+	(1,'active','spin'),
+	(2,'active','treadmill'),
+	(3,'active','yoga'),
+	(4,'active','core'),
+	(5,'active','life'),
+	(6,'active','spartan'),
+	(7,'active','crunch'),
+	(8,'active','bootcamp'),
+	(9,'active','peloton'),
+	(10,'inactive','couch potato');
+
+INSERT INTO sessions (id,gym_class_id,time_slot,maximum_bookings,available_bookings) VALUES
+	(1,1,'0900','3','2'),
+	(2,1,'1200','3','0'),
+	(3,1,'1600','4','4'),
+	(4,2,'1000','5','4'),
+	(5,2,'1200','2','2'),
+	(6,2,'1600','3','2'),
+	(7,3,'0900','4','3'),
+	(8,3,'1200','4','4'),
+	(9,3,'1600','5','4'),
+	(10,4,'0900','3','2');
+
+INSERT INTO bookings (id,member_id,session_id) VALUES
+	(1,1,1),
+	(2,1,2),
+	(3,1,3),
+	(4,2,2),
+	(5,2,4),
+	(6,2,6),
+	(7,3,2),
+	(8,3,7),
+	(9,3,10),
+	(10,4,9);
