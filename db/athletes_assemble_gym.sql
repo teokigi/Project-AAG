636aa2ea93effbb1888f4e3f8dac3908fd026611
Tony Tang <teokigi@hotmail.com>
2020-04-06 18:33:01 +0100

    found a work around seeding requirement

--- db/athletes_assemble_gym.sql
+++ db/athletes_assemble_gym.sql
@@ -30,50 +30,51 @@ CREATE TABLE bookings(
 	member_id INT REFERENCES members(id) ON DELETE CASCADE
 );
 
-INSERT INTO members (id,status,first_name,last_name,account_type) VALUES
-	(1,'active','Allan','Atkinson','standard'),
-	(2,'active','Brian','Bedford','standard'),
-	(3,'active','Charlie','Carlson','standard'),
-	(4,'active','David','Davidson','standard'),
-	(5,'active','Edward','Enders','standard'),
-	(6,'active','Fred','Flanders','standard'),
-	(7,'active','Greg','Granger','standard'),
-	(8,'active','Holly','Hanson','premium'),
-	(9,'active','Irene','Idlyeshire','premium'),
-	(10,'inactive','Jake','Jackson','standard');
 
-INSERT INTO gym_classes (id,status,name) VALUES
-	(1,'active','spin'),
-	(2,'active','treadmill'),
-	(3,'active','yoga'),
-	(4,'active','core'),
-	(5,'active','life'),
-	(6,'active','spartan'),
-	(7,'active','crunch'),
-	(8,'active','bootcamp'),
-	(9,'active','peloton'),
-	(10,'inactive','couch potato');
+INSERT INTO members (status,first_name,last_name,account_type) VALUES
+	('active','Allan','Atkinson','standard'),
+	('active','Brian','Bedford','standard'),
+	('active','Charlie','Carlson','standard'),
+	('active','David','Davidson','standard'),
+	('active','Edward','Enders','standard'),
+	('active','Fred','Flanders','standard'),
+	('active','Greg','Granger','standard'),
+	('active','Holly','Hanson','premium'),
+	('active','Irene','Idlyeshire','premium'),
+	('inactive','Jake','Jackson','standard');
 
-INSERT INTO sessions (id,gym_class_id,time_slot,maximum_bookings,available_bookings) VALUES
-	(1,1,'0900','3','2'),
-	(2,1,'1200','3','0'),
-	(3,1,'1600','4','4'),
-	(4,2,'1000','5','4'),
-	(5,2,'1200','2','2'),
-	(6,2,'1600','3','2'),
-	(7,3,'0900','4','3'),
-	(8,3,'1200','4','4'),
-	(9,3,'1600','5','4'),
-	(10,4,'0900','3','2');
+INSERT INTO gym_classes (status,name) VALUES
+	('active','spin'),
+	('active','treadmill'),
+	('active','yoga'),
+	('active','core'),
+	('active','life'),
+	('active','spartan'),
+	('active','crunch'),
+	('active','bootcamp'),
+	('active','peloton'),
+	('inactive','couch potato');
 
-INSERT INTO bookings (id,member_id,session_id) VALUES
-	(1,1,1),
-	(2,1,2),
-	(3,1,3),
-	(4,2,2),
-	(5,2,4),
-	(6,2,6),
-	(7,3,2),
-	(8,3,7),
-	(9,3,10),
-	(10,4,9);
+INSERT INTO sessions (gym_class_id,time_slot,maximum_bookings,available_bookings) VALUES
+	(1,'0900','3','2'),
+	(1,'1200','3','0'),
+	(1,'1600','4','4'),
+	(2,'1000','5','4'),
+	(2,'1200','2','2'),
+	(2,'1600','3','2'),
+	(3,'0900','4','3'),
+	(3,'1200','4','4'),
+	(3,'1600','5','4'),
+	(4,'0900','3','2');
+
+INSERT INTO bookings (member_id,session_id) VALUES
+	(1,1),
+	(1,2),
+	(1,3),
+	(2,2),
+	(2,4),
+	(2,6),
+	(3,2),
+	(3,7),
+	(3,10),
+	(4,9);
